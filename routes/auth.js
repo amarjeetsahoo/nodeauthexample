f6bd659dee614978eb6d83b5bf455842d4708f9a
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2020-12-31 19:59:49 +0530

    Implemented Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {registerValidation}=require('../validation');
 
-router.post('/register', async(req, res) => {
+
+router.post('/register', async (req, res) => {
+
+    //LET'S VALIDATE THE DATA BEFORE WE SEND TO DB
+    const { error } = registerValidation(req.body);
+
+    //const { error } = schema.validate(req.body);
+    // res.send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send("Email Exist");
+
+    //new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
