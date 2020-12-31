1a133732b5d8538b001348bd358d4aefaffc6a6a
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2020-12-31 20:48:20 +0530

    Encrption And Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const {registerValidation}=require('../validation');
+const { registerValidation } = require('../validation');
+const { loginValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/register', async (req, res) => {
@@ -10,23 +12,45 @@ router.post('/register', async (req, res) => {
 
     //const { error } = schema.validate(req.body);
     // res.send(error.details[0].message);
+
     if (error) return res.status(400).send(error.details[0].message);
 
+    //check email exist or not!!!
     const emailExist = await User.findOne({ email: req.body.email });
     if (emailExist) return res.status(400).send("Email Exist");
+
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
 
     //new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user:user._id});
     } catch (err) {
         res.status(400).send(err);
     }
+});
+
+//login
+router.post('/login', async(req, res) => {
+    //LET'S VALIDATE THE DATA BEFORE WE SEND TO DB
+    const { error } = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //check email exist or not!!!
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Email Doesn't Exist");
+    //password checking
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send("Invalid Password");
+
+    res.send("Logged In");
 });
 
 module.exports = router;
